3e557437e71fea4eeccc9eb551a5d01ce885b663
Tom <tom@mcclenahan.me>
2015-07-22 01:51:36 -0500

    Functions of buttons

--- keyboard.ino
+++ keyboard.ino
@@ -3,9 +3,24 @@
 
 static const uint8_t numButtons = 6;
 
+// Map buttons to notes in the scale
+/*
+  [0][1][2]
+  [5][4][3]
+
+  Buttons 5, 0, 1, 2, 3 will play a pentatonic scale.
+  Button 4 serves a special purpose.
+*/
+static const uint8_t buttonToNote[6] ={ 1, 2, 3, 4, 0, 0 };
+
+static const uint8_t playButton = 4;
+
 int lastStates[numButtons];
+int lastPressedStates[numButtons];
 long lastDebounceTimes[numButtons];
 long debounceDelay = 50;
+
+int songPos = 0;
 
 // Use MIDI over Serial1
 MIDI midi( Serial1 );
@@ -20,35 +35,54 @@ void setup()
   pinMode(5, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
   pinMode(7, INPUT_PULLUP);
-  
+  Serial.begin(9600);
   Serial1.begin(31250);
 }
 
 void loop()
 {
   int buttonState;
-  // Fashion a byte from the states of all six buttons
-  uint8_t dataByte = 0x00;
   uint8_t i;
   for( i = 0; i < numButtons; i++ )
   {
     buttonState = digitalRead(i+2);
-    // Set the ith bit if button pressed
     
     if( buttonState != lastStates[i] )
     {
       lastDebounceTimes[i] = millis();
     }
-    if( ( (millis() - lastDebounceTimes[i]) > debounceDelay ) )
+    if( ( (millis() - lastDebounceTimes[i]) > debounceDelay ) &&
+        ( lastPressedStates[i] != buttonState )
+    )
     {
-      if( buttonState == LOW )
-      {        
-        midi.noteOn(song[i]);
-      }
-      else
+      if( i != playButton )
       {
-        midi.noteOff(song[i]);
+        if( buttonState == LOW )
+        {        
+          midi.noteOn(scale[buttonToNote[i]]);
+          Serial.println(scale[buttonToNote[i]], DEC);
+        }
+        else
+        {
+          midi.noteOff(scale[buttonToNote[i]]);
+        }
       }
+      else if(songPos < songLength )
+      {
+        if( buttonState == LOW )
+        {
+          midi_chord chord = song[songPos++];        
+          midi.playChord(chord);
+          Serial.println(sizeof(midi_chord), DEC);
+        }
+        else
+        {
+          midi_chord chord = song[songPos-1];      
+
+          midi.releaseChord(chord);
+        }
+      }
+      lastPressedStates[i] = buttonState;
     }
     lastStates[i] = buttonState;
   }
